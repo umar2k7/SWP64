f2a3b62e009f752339b21adeb9f77a5092dd79cc
ocinboca <61325807+osmanof@users.noreply.github.com>
2025-06-29 23:31:56 +0300

    воодушевление

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
-async def root():
-    return {"message": "hey, swp64 project!!"}
+def read_root():
+    return {"message": "hey, swp64 project!!", "key": os.getenv("API_KEY")}
